64373dd397363757eeebcdad71555bf1841ef366
christian-bromann <contact@christian-bromann.com>
2016-07-21 15:48:23 +0200

    improved error handling of command hooks

---
+++ test/fixtures/error.handling.spec.js
@@ -0,0 +1,11 @@
+describe('dummy test', () => {
+    beforeAll(() => {
+    })
+
+    it('sample test', () => {
+        expect(browser.command()).toBe('foo')
+    })
+
+    afterAll(() => {
+    })
+})
